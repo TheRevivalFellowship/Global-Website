89535ba4fe48b685cc4b5866c4054491ac75636e
Bradley Flood <bradley.flood@gmail.com>
2015-12-31 15:10:44 +1100

    Init base controllers

---
+++ src/find-fellow/find-fellowship.controller.js
@@ -0,0 +1,13 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('FellowhsipFinder')
+        .controller('FindFellowshipController', FindFellowshipController);
+
+    function FindFellowshipController() {
+        var vm = this;
+
+    }
+
+})();
