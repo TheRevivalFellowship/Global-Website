c31bf8ba295f2558e2589d998e8073834a575d07
Bradley Flood <bradley.flood@gmail.com>
2016-01-09 23:22:32 +1100

    Adds a little styling and toggle functionality

--- src/find-fellow/find-fellowship.controller.js
+++ src/find-fellow/find-fellowship.controller.js
@@ -10,12 +10,17 @@
     function FindFellowshipController(FellowshipsModel, $timeout) {
         var vm = this,
             stateService,
-            fellowshipsData;
+            fellowshipsData,
+            classes;
 
         stateService = {
             // [default, locating, single, list]
             state: 'default'
-        }
+        };
+
+        classes = {
+            regionOpen: 'ship-list__region--open'
+        };
 
         fellowshipsData = FellowshipsModel.data;
 
@@ -30,6 +35,9 @@
         vm.useCurrentLocation = useCurrentLocation;
         vm.listAllFellowships = listAllFellowships;
         vm.close = close;
+        vm.toggleRegion = toggleRegion;
+        vm.openRegion = openRegion;
+        vm.closeRegion = closeRegion;
 
 
         /* ---------------------------------------- /*
@@ -50,14 +58,56 @@
 
         }
 
-        function close() {
+        function close(event) {
             stateService.state = 'default';
+        }
+
+        function openRegion(event, numParents) {
+            var region = getParent(event, numParents);
+
+            region.addClass(classes.regionOpen);
+        }
+
+        function toggleRegion(event, numParents) {
+            var region = getParent(event, numParents);
+
+            region.toggleClass(classes.regionOpen);
+        }
+
+        function closeRegion(event, numParents, stopProp) {
+            var region = getParent(event, numParents);
+
+            region.removeClass(classes.regionOpen);
+
+            if(stopProp) {
+                event.stopPropagation();
+            }
         }
 
 
         /* ---------------------------------------- /*
             PRIVATE
         /* ---------------------------------------- */
+
+        function getParent(event, numParents) {
+            var element, parent;
+
+            element = event.currentTarget;
+
+            parent = angular
+                .element(element)
+                .parent(); // 1
+
+            if(numParents == 2) {
+                parent = parent.parent(); // 2
+            }
+
+            if(numParents == 3) {
+                parent = parent.parent().parent(); // 3
+            }
+
+            return parent;
+        }
 
     }
 
