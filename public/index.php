59de6ab58270702350d9b8b044360ec4682227da
Bradley Flood <bradley.flood@gmail.com>
2016-04-22 17:33:08 +1000

    temporarily disable the FindAFellowship feature

--- public/index.php
+++ public/index.php
@@ -10,7 +10,7 @@ function inc_src($path) {
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-    <title>Revival Fellowship</title>
+    <title>The Revival Fellowship</title>
     <meta name="description" content="">
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -18,6 +18,28 @@ function inc_src($path) {
 
     <script src="https://use.typekit.net/ppd6ljk.js"></script>
     <script>try{Typekit.load({ async: true });}catch(e){}</script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular.min.js"></script>
+    <script src="assets/js/ngGeolocation.min.js"></script>
+    <script>
+        (function () {
+            'use strict';
+
+            angular
+                .module('trf', [
+
+                    'ngGeolocation'
+                    // ^^^ https://github.com/ninjatronic/ngGeolocation
+
+                ]);
+
+        })();
+    </script>
+    <script src="assets/js/website.min.js?lastCacheBust=2015.04.22"></script>
+
+    <script>(function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id='typef_orm',b='https://s3-eu-west-1.amazonaws.com/share.typeform.com/';if(!gi.call(d,id)){js=ce.call(d,'script');js.id=id;js.src=b+'share.js';q=gt.call(d,'script')[0];q.parentNode.insertBefore(js,q)}})()</script>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 
 </head>
 <body ng-controller="IndexController as ic" ng-class="'app-state--' + ic.state.get()">
@@ -30,28 +52,6 @@ function inc_src($path) {
         <?php inc_src('links/links.php'); ?>
         <?php inc_src('footer/footer.php'); ?>
     </main>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.0/angular.min.js"></script>
-    <script src="assets/js/ngGeolocation.min.js"></script>
-    <script>
-        (function () {
-            'use strict';
-
-            angular
-                .module('trf', [
-
-                    // https://github.com/ninjatronic/ngGeolocation
-                    'ngGeolocation'
-
-                ]);
-
-        })();
-    </script>
-    <script src="assets/js/website.min.js?lastCacheBust=2015.02.09"></script>
-
-    <script>(function(){var qs,js,q,s,d=document,gi=d.getElementById,ce=d.createElement,gt=d.getElementsByTagName,id='typef_orm',b='https://s3-eu-west-1.amazonaws.com/share.typeform.com/';if(!gi.call(d,id)){js=ce.call(d,'script');js.id=id;js.src=b+'share.js';q=gt.call(d,'script')[0];q.parentNode.insertBefore(js,q)}})()</script>
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 
 </body>
 </html>
