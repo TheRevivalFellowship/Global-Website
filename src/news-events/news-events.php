c7a00dc3e39df62f1825f421a32bedf1ca0f1019
Daniel Mee <daniel@danielmee.com>
2016-08-09 23:36:54 +0930

    Added vic rally info
    Was mainly doing something small to see if the CodeKit setup would
    work. Will need to do some changes in a scss file though to see if it
    really does work.

--- src/news-events/news-events.php
+++ src/news-events/news-events.php
@@ -6,15 +6,15 @@
     </div>
 
     <div class="news-events__main">
-        <img src="assets/img/content/event-life.jpg"
+        <img src="assets/img/content/event-victasrally.jpg"
             alt="Life Convention"
             class="news-events__main__img">
         <div class="news-events__main__info">
             <p class="news-events__main__info__copy">
-                Australian 2015 Convention, 6-8th June at the Gold Coast Convention Centre.
-                <a href="" class="hide-md">Register</a>
+                2016 biennial Victoria & Tasmania Rally, 15th & 16th October 2016 at the Bendigo Town Hall.
+                <a href="https://rf.churchapp.co.uk/events/xpdtoboe" class="hide-md">Register</a>
             </p>
-            <a class="news-events__main__info__action button button--alt show-md">Register</a>
+            <a href="https://rf.churchapp.co.uk/events/xpdtoboe" class="news-events__main__info__action button button--alt show-md">Register</a>
         </div>
     </div>
 </section>
